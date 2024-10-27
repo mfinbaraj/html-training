6fb44de4d115ebf863991dd5f7e4b79cab3eec5a
mfinbaraj <mfinbaraj@gmail.com>
2024-10-27 20:12:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,115 @@
+<html>
+	<head>
+		<title>My Learning</title>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+	</head>
+	<body>
+		<section id="header">
+			<img id="logo" src="./images/logo.png">
+			<h1 id="htitle">Shopping Cart</h1>
+			<div id="hmenu">
+				<ul id="hmenu-list">
+					<li class="hmenu-item active">Home</li>
+					<li class="hmenu-item">All Prodcuts</li>
+					<li class="hmenu-item">Best Offers</li>
+					<li class="hmenu-item">My Account</li>
+					<li class="hmenu-item">Contact Us</li>
+				</ul>
+			</div>
+			<div id="hright">
+				<a href="" class="hright-menu">Login / Sign UP</a>
+			</div>
+		</section>
+		<section id="banner">
+			<img id="banner-img" src="./images/banner.png" alt="Banner">
+		</section>
+		<section id="hcategory">
+			<h1 id="hcategory-title">Product Categories</h1>
+			<div id="hcontent">
+				<div class="hcontent-category hcleft">
+					<h1 class="hcontent-title">Electronics</h1>
+					<div class="hcontent-images">
+						<img src="./images/electronics.jpg" alt="Electronics" class="himages">
+					</div>
+					<div class="hcontent-data">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+					</div>
+				</div>
+				<div class="hcontent-category hccenter">
+					<h1 class="hcontent-title">Dresses</h1>
+					<div class="hcontent-images">
+						<img src="./images/dresses.jpg" alt="Dresses" class="himages">
+					</div>
+					<div class="hcontent-data">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+					</div>
+				</div>
+				<div class="hcontent-category hccenter">
+					<h1 class="hcontent-title">Books</h1>
+					<div class="hcontent-images">
+						<img src="./images/books.png" alt="Books" class="himages">
+					</div>
+					<div class="hcontent-data">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+					</div>
+				</div>
+				<div class="hcontent-category hcright">
+					<h1 class="hcontent-title">Footwear</h1>
+					<div class="hcontent-images">
+						<img src="./images/footwear.png" alt="Footwear" class="himages">
+					</div>
+					<div class="hcontent-data">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="contact">
+			<h1 class="ctitle"> Get In Touch </h1>
+			<div id="cform">
+				<form>
+					<div class="crow">
+						<input type="text" placeholder="Your Name" class="form-input">
+						<input type="text" placeholder="Your Phone Number" class="form-input">
+					</div>
+					<div class="crow">
+						<input type="text" placeholder="Your Email" class="form-input">
+						<select class="form-input">
+							<option>Please Select</option>
+							<option>Request</option>
+							<option>Suggestion</option>
+							<option>Complaint</option>
+						</select>
+					</div>
+					<div class="crow">
+						<textarea placeholder="Write Here" class="form-input input-commands"> </textarea>
+					</div>
+					<div class="crow">
+						<input type="button" Value="Submit" class="form-submit">
+					</div>
+				</form>			
+			</div>
+		</section>
+		<section id="location">
+			<h1 id="ltitle"> Locate Us</h1>
+			<div id="cmap">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31510.601388558593!2d77.85203909569803!3d9.170238217383108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b06b26570998f29%3A0xd95cda8fab23619b!2sKovilpatti%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1730022331501!5m2!1sen!2sin" width="100%" height="650" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+		</section>
+		<section id="footer">
+			<div id="follow">
+				Follow Us On
+			</div>
+			<div id="fcopy-right">
+				<i class="fa-solid fa-copyright"></i> Copy Right. All Rights Reserved.
+			</div>
+			<div id="social-media">
+				<i class="fa-brands fa-facebook ficons"></i>
+				<i class="fa-brands fa-twitter ficons"></i>
+				<i class="fa-brands fa-square-instagram ficons"></i>
+				<i class="fa-brands fa-linkedin ficons"></i>
+			</div>
+		</section>
+	</body>
+</html>
